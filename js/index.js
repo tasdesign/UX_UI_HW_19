5c860befb01cf48a603595a5e8e2a7a519a07ae8
Tasfia2001 <tasfiagoznovi@gmail.com>
2023-07-30 11:30:25 +0100

    New hw19 with responsiveness

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
 console.log("Your index.js file is loading correctly!");
-console.log('hello');
+
+
+//Scroll Animation
+
+const body = document.body;
+			const progressBar = document.querySelector('.progress__bar');
+			console.log(progressBar)
+            const updateProgress = () => {
+				let scrollPos =
+					(window.scrollY / (body.scrollHeight - window.innerHeight)) * 100;
+                console.log(scrollPos)
+                progressBar.style.width = `${scrollPos}%`;
+				requestAnimationFrame(updateProgress);
+			};
+
+			updateProgress();
+
+// 
+
+
